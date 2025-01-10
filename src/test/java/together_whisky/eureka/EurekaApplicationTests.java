72801e6d2b80c145fc57d144a1ceb4609f4174ee
wook <mystyle730@gmail.com>
2025-01-10 18:39:47 +0900

    init

---
+++ src/test/java/together_whisky/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package together_whisky.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
